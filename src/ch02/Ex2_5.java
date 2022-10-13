711ae810fe626a4bd80ae180c6bfa00e76f4b679
cse1940 <se1940@naver.com>
2022-10-13 22:20:43 +0900

    221013_upload

---
+++ src/ch02/Ex2_5.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class Ex2_5 {
+
+	public static void main(String[] args) {
+		
+		int x = 10; // 변수에 다른 값을 저장하기만 하면 된다
+		int y = 5; // 변수에 다른 값을 저장하기만 하면 된다
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+
+	}
+
+}
