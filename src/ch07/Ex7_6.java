78d8247f74d1d4b715705bbd35040c0a0cca132c
cse1940 <se1940@naver.com>
2022-10-18 23:34:45 +0900

    221018_upload

---
+++ src/ch07/Ex7_6.java
@@ -0,0 +1,19 @@
+package ch07;
+
+import static java.lang.System.out; // System.out을 out만으로 참조가능
+import static java.lang.Math.*;     // Math
+
+public class Ex7_6 {
+
+	public static void main(String[] args) {
+		
+//		System.out.println(Math.random());
+		out.println(random());
+
+//		System.out.println("Math.PI :" + Math.PI);
+		out.println("Math.PI :" + PI);
+	}
+
+	
+
+}
---
+++ src/ch07/Ex7_7.java
@@ -0,0 +1,37 @@
+package ch07;
+
+public class Ex7_7 {
+
+	public static void main(String[] args) {
+		
+		Car car = null;
+		FireEngine fe = new FireEngine();
+		FireEngine fe2 = null;
+
+		fe.water();
+		car = fe; // car = (Car)fe; 형변환이 생략됨
+//		car.water(); -> error, Car타입의 참조변수로는 water()를 호출할 수 없다
+		fe2 = (FireEngine)car; // 자손타입 <- 조상타입, 형변환 생략 불가
+		fe2.water();
+	}
+
+}
+
+class Car {
+	String color;
+	int door;
+	
+	void drive() { // 운전하는 기능
+		System.out.println("drive, Brrr~");
+	}
+	
+	void stop() {  // 멈추는 기능
+		System.out.println("stop!!");
+	}
+}
+
+class FireEngine extends Car { // 소방차
+	void water() { // 물을 뿌리는 기능
+		System.out.println("water!!");
+	}
+}
