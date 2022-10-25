f4b6884fa5471e465cd46d98b41122921cee9378
cse1940 <se1940@naver.com>
2022-10-25 14:38:17 +0900

    221025_modify

--- src/ch07/Ex7_18.java
+++ src/ch07/Ex7_18.java
@@ -6,13 +6,14 @@ import java.awt.event.*;
 class Ex7_18 {
 	public static void main(String[] args) {
 		Button b = new Button("Start");
-		b.addActionListener(new ActionListener() { // 클래스의 정의와 객체 생성을 동시에
-		    public void actionPerformed(ActionEvent e) {
-	        System.out.println("ActionEvent occurred!!!");
-		    }
-		});
-  
+		b.addActionListener(new EventHandler()); // 클래스의 정의와 객체 생성을 동시에
 	}
+}
+
+class EventHandler implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("ActionEvent occurred!!!");
+    }
 }
 
 
