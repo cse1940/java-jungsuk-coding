1718a2ab800a29c40091db0412e3e87b47233c01
cse1940 <se1940@naver.com>
2022-10-26 16:13:55 +0900

    221026_upload

---
+++ src/ch08/ExceptionTest.java
@@ -0,0 +1,12 @@
+package ch08;
+
+public class ExceptionTest {
+
+    public static void main(String[] args) {
+        
+        System.out.println(args[0]); // 런타임 error, 실행 중 발생하는 에러
+//      system.out.println(args[0]); -> 컴파일 error, 컴파일 할 때 발생하는 에러
+
+    }
+
+}
