9db0b6881493cb3fd6a05505005c81757f0b59c2
cse1940 <se1940@naver.com>
2022-10-26 16:24:59 +0900

    221026_modify

--- src/ch08/Ex8_4.java
+++ src/ch08/Ex8_4.java
@@ -8,13 +8,13 @@ public class Ex8_4 {
         System.out.println(2);
         try {
             System.out.println(3);
-            System.out.println(0/0);
+            System.out.println(0/0); // 0으로 나눠서 고의로 ArithmeticException을 발생시킨다.
             System.out.println(4); // 실행되지 않는다.
         } catch(ArithmeticException ae) {
             if(ae instanceof ArithmeticException)
                 System.out.println("true");
             System.out.println("ArithmeticException");
-        } catch(Exception e) {
+        } catch(Exception e) { // ArithmeticException을 제외한 모든 예외가 처리된다.
             System.out.println("Exception");
         }
         System.out.println(6);
