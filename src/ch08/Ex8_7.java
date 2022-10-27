1147caaa494015195d645ead05052da17c6266ba
cse1940 <se1940@naver.com>
2022-10-27 23:30:06 +0900

    221027_upload

--- src/ch08/Ex8_7.java
+++ src/ch08/Ex8_7.java
@@ -3,8 +3,8 @@ package ch08;
 public class Ex8_7 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        
+//      throw new Exception(); -> 예외 처리 안함(try-catch)
     }
 
 }
