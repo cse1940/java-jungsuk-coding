a544f363fc142ef1c35da82837eea9bc29c3ad1d
cse1940 <se1940@naver.com>
2022-10-27 23:30:11 +0900

    221027_upload

--- src/ch08/Ex8_8.java
+++ src/ch08/Ex8_8.java
@@ -3,7 +3,8 @@ package ch08;
 public class Ex8_8 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        
+        throw new RuntimeException(); // RuntimeException을 고의로 발생시킨다.
 
     }
 
