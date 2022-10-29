f578baf717c0fba4c27b46e0e008323ea03297e5
cse1940 <se1940@naver.com>
2022-10-29 18:40:49 +0900

    221029_upload

---
+++ src/ch09/Ex9_7.java
@@ -0,0 +1,16 @@
+package ch09;
+
+public class Ex9_7 {
+
+    public static void main(String[] args) {
+        
+//      이 예제를 실행하면 "AAA"라는 문자열을 담고 있는 String인스턴스가 하나 생성된 후, 
+//      참조변수 s1, s2, s3는 모두 이 String인스턴스를 참조하게 된다.
+        String s1 = "AAA";
+        String s2 = "AAA";
+        String s3 = "AAA";
+        String s4 = "BBB";
+
+    }
+
+}
