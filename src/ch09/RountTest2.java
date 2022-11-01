e1483331713dbfc740f35189a504ef7f5090c999
cse1940 <se1940@naver.com>
2022-11-01 11:49:43 +0900

    221101_upload

---
+++ src/ch09/RountTest2.java
@@ -0,0 +1,15 @@
+package ch09;
+
+public class RountTest2 {
+
+    public static void main(String[] args) {
+        
+//      int i = Integer.parseInt("100");
+        System.out.println("i=" + Integer.parseInt("100"));
+        System.out.println("i=" + Integer.parseInt("100", 10)); // 위와 동일
+        System.out.println("i=" + Integer.parseInt("100", 2)); // 2진수
+        System.out.println("i=" + Integer.parseInt("FF", 16)); // 16진수
+
+    }
+
+}
