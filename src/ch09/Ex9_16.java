1137fb664c8d8f5b01c7301877d4ce17cc5abd8a
cse1940 <se1940@naver.com>
2022-11-02 12:23:07 +0900

    221102_upload

--- src/ch09/Ex9_16.java
+++ src/ch09/Ex9_16.java
@@ -17,6 +17,18 @@ public class Ex9_16 {
         
         Integer intg2 = new Integer(20);
         int i3 = (int)intg2; // 참조형을 기본형으로 형변환도 가능(형변환 생략가능)
+        
+        Integer intg3 = intg2 + i3;
+        
+        System.out.println("i = " + i);
+        System.out.println("intg = " + intg);
+        System.out.println("obj = " + obj);
+        System.out.println("lng = " + lng);
+        System.out.println("intg + 10 = " + i2);
+        System.out.println("intg + lng = " + l);
+        System.out.println("intg2 = " + intg2);
+        System.out.println("i3 = " + i3);
+        System.out.println("intg2 + i3 = " + intg3);
     }
 
 }
