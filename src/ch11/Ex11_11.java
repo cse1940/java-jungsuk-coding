429b776e8f6b37d51f7c05950157852e39481501
cse1940 <se1940@naver.com>
2022-11-04 14:37:28 +0900

    221104_modify

--- src/ch11/Ex11_11.java
+++ src/ch11/Ex11_11.java
@@ -1,6 +1,7 @@
 package ch11;
 
 import java.util.HashSet;
+import java.util.Objects;
 
 public class Ex11_11 {
 
@@ -8,7 +9,7 @@ public class Ex11_11 {
         
         HashSet set = new HashSet();
         set.add("abc");
-        set.add("abc");
+        set.add("abc"); // 중복이라 저장X
         set.add(new Person("David", 10));
         set.add(new Person("David", 10));
         
@@ -17,7 +18,25 @@ public class Ex11_11 {
 
 }
 
+//  equals()와 hashCode()를 오버라이딩해야 HashSet이 바르게 동작함
 class Person {
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        return age == other.age && Objects.equals(name, other.name);
+    }
+
     String name;
     int age;
     
